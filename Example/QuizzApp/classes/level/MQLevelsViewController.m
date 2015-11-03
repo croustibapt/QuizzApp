0957207960cd97dca53b2875e4e69b27a36350a4
Baptiste LE GUELVOUIT <baptiste.leguelvouit@gmail.com>
2015-11-03 08:08:40 +0100

    Start removing MQ1 specific stuff

--- Example/QuizzApp/classes/level/MQLevelsViewController.m
+++ Example/QuizzApp/classes/level/MQLevelsViewController.m
@@ -14,68 +14,15 @@ int const MQ_RELEASE_DATE = 1415612737;
 #import "MQConstants.h"
 #import "GameDBHelper.h"
 
-@interface MQLevelsViewController () {
-    CGRect m_startCollectionViewFrame;
-    FlatButton * m_flatButton;
-}
-
+@interface MQLevelsViewController ()
 @end
 
 @implementation MQLevelsViewController
 
 USERPREF_IMPL(NSNumber *, HasLocalLevels, [NSNumber numberWithBool:NO]);
 
-- (void)checkLocalLevels {
-    Boolean hasLocalLevels = [GameDBHelper hasLevelsWithMaxId:MOVIE_QUIZZ_IOS_START_ID];
-    NSInteger nbLevels = [self.levels count];
-    
-    //Only 1 level in "new" local
-    if ((nbLevels <= 1) && ![[MQLevelsViewController getHasLocalLevels] boolValue]) {
-        NSDate * releaseDate = [[NSDate alloc] initWithTimeIntervalSince1970:MQ_RELEASE_DATE];
-        NSDate * date = [NSDate date];
-        
-        float timeInterval = [date timeIntervalSinceDate:releaseDate];
-        
-        hasLocalLevels = (timeInterval > 0);
-    } else {
-        [MQLevelsViewController setHasLocalLevels:[NSNumber numberWithBool:YES]];
-    }
-    
-    if (hasLocalLevels) {
-        [m_flatButton setHidden:NO];
-    } else {
-        [m_flatButton setHidden:YES];
-    }
-}
-
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    //
-    CGRect oldFrame = self.collectionView.frame;
-    m_startCollectionViewFrame = CGRectMake(oldFrame.origin.x, oldFrame.origin.y, oldFrame.size.width, oldFrame.size.height - 40);
-    
-    [self.collectionView setFrame:m_startCollectionViewFrame];
-//    [self.collectionView setBackgroundColor:[UIColor redColor]];
-    
-    float widthPart = m_startCollectionViewFrame.size.width / 6.0;
-    CGRect buttonFrame = CGRectMake(widthPart, m_startCollectionViewFrame.size.height + 4, 4 * widthPart, 32);
-    
-    //Local levels button
-    m_flatButton = [[FlatButton alloc] initWithFrame:buttonFrame];
-    [m_flatButton setHidden:YES];
-    
-    [m_flatButton setFrontColor:QUIZZ_APP_ORANGE_MAIN_COLOR];
-    [m_flatButton setBackColor:QUIZZ_APP_ORANGE_SECOND_COLOR];
-    
-    [m_flatButton setAutoresizingMask:UIViewAutoresizingFlexibleTopMargin];
-    [m_flatButton setTitle:NSLocalizedStringFromTableInBundle(@"STR_OLD_LEVELS", nil, QUIZZ_APP_STRING_BUNDLE, nil) forState:UIControlStateNormal];
-    
-    [m_flatButton addTarget:self action:@selector(onLocalLevelsButtonPush:) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:m_flatButton];
-    
-    //
-    [self checkLocalLevels];
 }
 
 - (void)threadLoadLocalLevels {
@@ -114,9 +61,6 @@ USERPREF_IMPL(NSNumber *, HasLocalLevels, [NSNumber numberWithBool:NO]);
     
     //And reload collection view
     [self.collectionView reloadData];
-    
-    //
-    [self checkLocalLevels];
     
     if (hud) {
         [HUD hide:YES];
