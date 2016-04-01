5a188f2964da9eef88726f0687fb53f148435ab9
Baptiste LE GUELVOUIT <baptiste.leguelvouit@gmail.com>
2016-04-01 08:33:36 +0200

    Clean constants

--- Example/QuizzApp/classes/common/MQConstants.h
+++ Example/QuizzApp/classes/common/MQConstants.h
@@ -6,37 +6,43 @@
 //  Copyright (c) 2014 Baptiste LE GUELVOUIT. All rights reserved.
 //
 
+
 #import <Foundation/Foundation.h>
+
 
 #import "Constants.h"
 #import "UtilsColors.h"
 
+
 extern NSString * const MOVIE_QUIZZ_GOOGLE_ANALYTICS_ID;
+
 
 extern NSString * const MOVIE_QUIZZ_APP_ID;
 
-extern NSString * const MOVIE_QUIZZ_GOOGLE_PLAY_CLIENT_ID;
-
-extern NSString * const MOVIE_QUIZZ_GOOGLE_PLAY_PROGRESSION_KEY;
-
-extern NSString * const MOVIE_QUIZZ_GOOGLE_PLAY_LEADER_BOARD_ID;
 
 extern NSString * const MOVIE_QUIZZ_SERVICE_NAME;
 
+
 extern int const MOVIE_QUIZZ_IOS_START_ID;
+
 
 extern NSString * TV_SHOW_QUIZZ_APP_ID;
 
+
 extern NSString * ADDICT_APP_ID;
 
-#define MOVIE_QUIZZ_MAIN_COLOR UIColorFromRGB(0x03a9f4)
-#define MOVIE_QUIZZ_SECOND_COLOR UIColorFromRGB(0x29b6f6)
-#define MOVIE_QUIZZ_THIRD_COLOR UIColorFromRGB(0x4fc3f7)
 
-#define MOVIE_QUIZZ_OPPOSITE_MAIN_COLOR UIColorFromRGB(0xffa000)
-#define MOVIE_QUIZZ_OPPOSITE_SECOND_COLOR UIColorFromRGB(0xffc107)
-#define MOVIE_QUIZZ_OPPOSITE_THIRD_COLOR UIColorFromRGB(0xffd54f)
+#define MOVIE_QUIZZ_MAIN_COLOR              UIColorFromRGB(0x03a9f4)
+#define MOVIE_QUIZZ_SECOND_COLOR            UIColorFromRGB(0x29b6f6)
+#define MOVIE_QUIZZ_THIRD_COLOR             UIColorFromRGB(0x4fc3f7)
+
+
+#define MOVIE_QUIZZ_OPPOSITE_MAIN_COLOR     UIColorFromRGB(0xffa000)
+#define MOVIE_QUIZZ_OPPOSITE_SECOND_COLOR   UIColorFromRGB(0xffc107)
+#define MOVIE_QUIZZ_OPPOSITE_THIRD_COLOR    UIColorFromRGB(0xffd54f)
+
 
 @interface MQConstants : NSObject
+
 
 @end
